d05db1430890fad9478e1503f73681d52bc865cc
Yann BARRAULT <ybarrault@timwi.fr>
2019-02-28 20:47:03 +0100

    Add Slide class

---
+++ helloworld-mdb/src/main/java/hashcode/model/Slide.java
@@ -0,0 +1,32 @@
+package hashcode.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Slide {
+	
+	private Photo premierePhoto;
+	
+	private Photo secondePhoto;
+	
+	private List<String> tags = new ArrayList<String>();
+	
+	public Photo getPremierePhoto() {
+		return premierePhoto;
+	}
+	public void setPremierePhoto(Photo premierePhoto) {
+		this.premierePhoto = premierePhoto;
+	}
+	public Photo getSecondePhoto() {
+		return secondePhoto;
+	}
+	public void setSecondePhoto(Photo secondePhoto) {
+		this.secondePhoto = secondePhoto;
+	}
+	public List<String> getTags() {
+		return tags;
+	}
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+}
