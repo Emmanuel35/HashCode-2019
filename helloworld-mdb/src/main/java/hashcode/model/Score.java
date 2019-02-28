fb7f5914323c66fe312f0828dd268d194089d320
Yann BARRAULT <ybarrault@timwi.fr>
2019-02-28 22:13:08 +0100

    next vert slide fixed

--- helloworld-mdb/src/main/java/hashcode/model/Score.java
+++ helloworld-mdb/src/main/java/hashcode/model/Score.java
@@ -10,7 +10,6 @@ public class Score {
 		int nbUniqueTagsCurrent = 0;
 		int nbUniqueTagsNext = 0;
 		int nbUniquesCommuns = 0;
-<<<<<<< Updated upstream
 				
 		List<String> listeUniqueCurrent = new ArrayList<>(current.getTags());
 		List<String> listeUniqueNext = new ArrayList<>(next.getTags());
@@ -52,13 +51,5 @@ public class Score {
 		}
 		
 		return slide;
-=======
-		nbUniqueTagsCurrent = current.getTags().size();
-				
-		//listeTagsCommuns = current.getTags()
-		//- current.getTags().r
-		
-		return Math.min(nbUniquesCommuns, Math.min(nbUniqueTagsCurrent, nbUniqueTagsnext));
->>>>>>> Stashed changes
 	}
 }
