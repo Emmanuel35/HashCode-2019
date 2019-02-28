b6c8f4b9f064bafd324928e53b5f8a67a25ce5d9
Yann BARRAULT <ybarrault@timwi.fr>
2019-02-28 21:08:32 +0100

    Score update

--- helloworld-mdb/src/main/java/hashcode/model/Score.java
+++ helloworld-mdb/src/main/java/hashcode/model/Score.java
@@ -1,12 +1,33 @@
 package hashcode.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Score {
 	
-	public static int computeScore(Photo[] current, Photo[] next) {
+	public static int computeScore(Slide current, Slide next) {
 		// nombre de tags uniquement présent dans le slide current
 		int nbUniqueTagsCurrent = 0;
-		int nbUniqueTagsnext = 0;
+		int nbUniqueTagsNext = 0;
 		int nbUniquesCommuns = 0;
-		return Math.min(nbUniquesCommuns, Math.min(nbUniqueTagsCurrent, nbUniqueTagsnext));
+		nbUniqueTagsCurrent = current.getTags().size();
+				
+		List<String> listeUniqueCurrent = new ArrayList<String>(current.getTags());
+		List<String> listeUniqueNext = new ArrayList<String>(next.getTags());
+		List<String> listeCommune = new ArrayList<String>(current.getTags());
+		List<String> tempListe = new ArrayList<String>(next.getTags());
+		listeCommune.retainAll(tempListe);
+		
+		// on retire ce qui est commun
+		listeUniqueCurrent.removeAll(next.getTags());
+		// on retire ce qui est commun
+		listeUniqueNext.removeAll(current.getTags());
+		
+		
+		nbUniqueTagsCurrent = listeUniqueCurrent.size();
+		nbUniqueTagsNext = listeUniqueNext.size();
+		nbUniquesCommuns = listeCommune.size();
+		
+		return Math.min(nbUniquesCommuns, Math.min(nbUniqueTagsCurrent, nbUniqueTagsNext));
 	}
 }
